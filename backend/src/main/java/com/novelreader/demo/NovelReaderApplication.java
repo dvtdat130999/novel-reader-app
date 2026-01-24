316f61df38d2b836847723c082a777e971b9f7fb
Dat <tdat130999@gmail.com>
2026-01-24 10:25:23 +0700

    devops: add workflow github action CI pipeline

--- backend/src/main/java/com/novelreader/demo/NovelReaderApplication.java
+++ backend/src/main/java/com/novelreader/demo/NovelReaderApplication.java
@@ -1,10 +1,12 @@
 package com.novelreader.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
 import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
+@Slf4j
 @SpringBootApplication(exclude = {
         SecurityAutoConfiguration.class,
         UserDetailsServiceAutoConfiguration.class
@@ -13,7 +15,7 @@ public class NovelReaderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NovelReaderApplication.class, args);
-		System.out.println("Running.");
+		log.info("Running.");
 	}
 
 }
